fdd25bc729b0df5d4abdb8728241fad059ca9ebe
stormman <yuguangliang001@163.com>
2016-02-16 13:39:50 +0800

    添加推送1，5，15，30，60，收市时间到策略。

--- easyquant/event_type.py
+++ easyquant/event_type.py
@@ -1,8 +1,9 @@
 # coding: utf-8
-EVENT_TIMER = 'second_timer'
-EVENT_QUOTATION = 'quotation'
+#EVENT_TIMER = 'second_timer'
+#EVENT_QUOTATION = 'quotation'
 
 
 class EventType:
     TIMER = 'second_timer'
     QUOTATION = 'quotation'
+    CLOCK = "clock_tick";
