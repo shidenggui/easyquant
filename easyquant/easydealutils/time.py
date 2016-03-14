b108f4d2440e557f3e2c15e3b2744aa19585cfe0
shidenggui <longlyshidenggui@gmail.com>
2016-03-14 16:33:47 +0800

    fix(easydealutils): fix close time

--- easyquant/easydealutils/time.py
+++ easyquant/easydealutils/time.py
@@ -35,7 +35,7 @@ def is_holiday_today():
 def is_tradetime_now():
     now_time = time.localtime()
     now = (now_time.tm_hour, now_time.tm_min, now_time.tm_sec)
-    if (9, 15, 0) <= now <= (11, 30, 0) or (13, 0, 0) <= now <= (25, 0, 0):
+    if (9, 15, 0) <= now <= (11, 30, 0) or (13, 0, 0) <= now <= (15, 0, 0):
         return True
     return False
 
