765bd1735dc9577143182743265720ae97986b96
marzwu <marzwu@gmail.com>
2016-04-09 11:57:44 +0800

    修复：由可变参数列表调用is_holiday会传入的日期被转成列表，用此列表当key取返回结果的json数据报错

--- easyquant/easydealutils/time.py
+++ easyquant/easydealutils/time.py
@@ -24,7 +24,7 @@ def is_holiday(day):
     api = 'http://www.easybots.cn/api/holiday.php'
     params = {'d': day}
     rep = requests.get(api, params)
-    res = rep.json()[day]
+    res = rep.json()[day if isinstance(day, str) else day[0]]
     return True if res == "1" else False
 
 
